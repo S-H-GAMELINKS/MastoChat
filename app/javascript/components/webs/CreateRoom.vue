c375c1b97e7f199a295a0f53f5edd1d27e33928b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-15 22:59:16 +0900

    add create chat room form

--- app/javascript/components/webs/CreateRoom.vue
+++ app/javascript/components/webs/CreateRoom.vue
@@ -1,6 +1,15 @@
 <template>
 <div>
     <h1>Create Chat Room Pages</h1>
+    <div class="input-group">
+        <div class="input-group-append">
+            <span class="input-group-text">ルーム名</span>
+        </div>
+        <input type="text" class="form-control" v-model="title" placeholder="ルーム名を入力してください"> 
+    </div>
+    <p>
+        <button type="button" class="btn btn-primary" v-on:click="createChatRoom">Submit</button>
+    </p>
     <div v-for="(room, key, index) in rooms" :key="index">
         {{room.title}}
     </div>
