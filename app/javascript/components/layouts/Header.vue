35223aff16e9da15171ceb5f128de7723695e35a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-15 13:37:54 +0900

    add NavBar

---
+++ app/javascript/components/layouts/Header.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">MastoChat</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</div>
+</template>
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue/dist/vue.esm';
+import Header from '../components/layouts/Header.vue';
 
 const app = new Vue({
     el: "#app",
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             text: "Hello Rails and Vue.js!"
