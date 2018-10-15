9d8f4d97864a680d22efbdf509677fbb73b85ea9
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-15 17:50:43 +0900

    add set_chat & before_action

--- app/controllers/api/chat_controller.rb
+++ app/controllers/api/chat_controller.rb
@@ -1,7 +1,13 @@
 class Api::ChatController < ActionController::API
+    before_action :set_chat, only: [:show, :edit, :update, :destory]
 
     def index
         @chats = Chat.all
         render json: @chats
     end
+
+    private
+        def set_chat
+            @chat = Chat.find(params[:id])
+        end
 end
