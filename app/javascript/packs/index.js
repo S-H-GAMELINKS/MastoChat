b35503e25bd8c4b65ed4961f1025de719b476e35
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-15 13:57:40 +0900

    using vue-router

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue/dist/vue.esm';
+import Router from '../router/router';
 import Header from '../components/layouts/Header.vue';
 
 const app = new Vue({
     el: "#app",
+    router: Router,
     components: {
         'nav-bar': Header
     },
