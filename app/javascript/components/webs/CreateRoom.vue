b0e79be96f098caafe4f7912263933932598efea
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-15 22:30:06 +0900

    show rooms

--- app/javascript/components/webs/CreateRoom.vue
+++ app/javascript/components/webs/CreateRoom.vue
@@ -1,5 +1,19 @@
 <template>
 <div>
     <h1>Create Chat Room Pages</h1>
+    <div v-for="(room, key, index) in rooms" :key="index">
+        {{room.title}}
+    </div>
 </div>
 </template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            rooms: []
+        }
+    }    
+}
+</script>
+
