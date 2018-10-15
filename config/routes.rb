230c114844bb86221d0702f3dc48a0e80bfa5122
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-15 17:32:09 +0900

    add API route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get 'web/index'
   get '/about',   to: 'web#index'
   get '/contact',   to: 'web#index'
+
+  namespace :api, format: 'json' do
+    resources :chat
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
