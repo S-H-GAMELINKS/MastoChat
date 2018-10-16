6f7c0950566202838de7a9b709c77ecf00a16bf7
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-16 23:00:07 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'web#index'
   get 'web/index'
   get '/about',   to: 'web#index'
