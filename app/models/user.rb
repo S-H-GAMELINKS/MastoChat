7e8ec4e2497ebe9bda5e858d4c1d239dde00e48a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-16 21:00:07 +0900

    add self.dummy_email method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable
+    :recoverable, :rememberable, :validatable, :omniauthable
+
+  private
+    def self.dummy_email(auth)
+      "#{auth.uid}"
+    end
 end
