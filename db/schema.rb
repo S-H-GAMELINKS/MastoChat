606a3049525c75f8078af56a9b0fb27d01f913fd
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-16 18:36:55 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_16_075847) do
+ActiveRecord::Schema.define(version: 2018_10_16_075922) do
 
   create_table "chats", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mastodon_clients", force: :cascade do |t|
+    t.string "domain"
+    t.string "client_id"
+    t.string "client_secret"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
