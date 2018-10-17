432ee564b178ba826c86c39202a9cc78a35f0f83
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-17 14:22:36 +0900

    add about

--- app/javascript/components/webs/About.vue
+++ app/javascript/components/webs/About.vue
@@ -1,5 +1,9 @@
 <template>
 <div>
     <h1>About Pages</h1>
+    <div class="container">
+        <p>Mastodonアカウントで使用できるオープンなWebチャットサービスです</p>
+        <p>Mastodonに関する質問や相談などを気軽にオープンに話せる場が欲しいと思い作りました。</p>
+    </div>
 </div>
 </template>
